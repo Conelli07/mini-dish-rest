cb69095a9f2b9533a8b1c8097f045fc6dbb82eff
conelli BISKUPOVIC <conelliconi1200@gmail.com>
2026-03-28 19:10:45 +0300

    Initial project structure for TD5

---
+++ src/main/java/com/restaurant/minidish/MiniDishRestApplication.java
@@ -0,0 +1,12 @@
+package com.restaurant.minidish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDishRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDishRestApplication.class, args);
+    }
+}
